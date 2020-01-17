e9d4c74548b0b77e0f856e3738cf3be1e30fc01b
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-17 22:25:08 +0300

    Add files via upload

---
+++ Day 25/Path Sum - better.cpp
@@ -0,0 +1,30 @@
+﻿// Question Link ---> https://leetcode.com/problems/path-sum/
+// Path Sum
+// Day #25
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL) {
+            return false;
+        }
+
+        return helper(root, sum, 0);
+    }
+    
+    bool helper(TreeNode* root, int sum, int pathSum) {
+        if (root != NULL) {
+            pathSum += root->val;
+        }
+
+        if (root && root->left == NULL && root->right == NULL && pathSum == sum) { // Leaf
+            return true;
+        }
+
+        if (root != NULL) {
+            return helper(root->left, sum, pathSum) 
+            || helper(root->right, sum, pathSum);
+        }
+        return false;
+    }
+};
---
+++ Day 25/Path Sum - ver2.cpp
@@ -0,0 +1,18 @@
+﻿// Question Link ---> https://leetcode.com/problems/path-sum/
+// Path Sum
+// Day #25
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL) {
+            return false;
+        }
+
+        if (!root->left && !root->right && root->val == sum) {
+            return true;
+        }
+        
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
