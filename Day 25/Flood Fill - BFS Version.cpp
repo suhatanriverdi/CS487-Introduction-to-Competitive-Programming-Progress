947ab7a9650a778a16b8762c0c5912e104765e6a
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-17 20:49:43 +0300

    Add files via upload

---
+++ Day 25/Flood Fill - BFS Version.cpp
@@ -0,0 +1,51 @@
+﻿// Question Link ---> https://leetcode.com/problems/flood-fill/
+// Flood Fill
+// Day #25
+#include <iostream>    
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+		queue<pair<int, int>> pixelCoordinates;
+		vector<vector<bool>> isPainted(image.size(), vector<bool>(image[0].size(), false));
+		pixelCoordinates.push({sr, sc});
+		int originalColor = image[sr][sc]; // We use this for checking visited / not-visited
+		image[sr][sc] = newColor; // We filled start pixel with newColor (marked as visited)
+		isPainted[sr][sc] = true; // Marked as filled
+		while (!pixelCoordinates.empty()) {
+			pair<int, int> currentPixel = pixelCoordinates.front();
+			pixelCoordinates.pop();
+			// Search 4-Directions
+			pair<int, int> directions[4] = { {0, 1}, {0, -1}, {-1, 0}, {1, 0} };
+			for (auto direction : directions) {
+				int newX = direction.first + currentPixel.first;
+				int newY = direction.second + currentPixel.second;
+				if (newX >= 0 && newX < image.size() && newY >= 0 && newY < image[0].size() && image[newX][newY] == originalColor && !isPainted[newX][newY]) {
+					image[newX][newY] = newColor; // Fill pixel with new color
+					isPainted[newX][newY] = true;
+					pixelCoordinates.push({ newX, newY }); // Save new filled pixel for neighboring pixels
+				}
+			}
+		}
+		return image;
+	}
+};
+
+int main(void) {
+	vector<vector<int>> res, image = { {0, 0, 0}, {0, 1, 1} };
+	int sr = 1, sc = 1, newColor = 1;
+	//vector<vector<int>> res, image = { {1, 1, 1}, {1, 1, 0}, {1, 0, 1} };
+	//int sr = 1, sc = 1, newColor = 2;
+	Solution obj;
+	res = obj.floodFill(image, sr, sc, newColor);
+	cout << "Result: \n";
+	for (auto row : image) {
+		for (auto pixel : row) {
+			cout << pixel << " ";
+		}
+		cout << endl;
+	}
+}
---
+++ Day 25/Flood Fill - DFS Version (Better).cpp
@@ -0,0 +1,45 @@
+﻿// Question Link ---> https://leetcode.com/problems/flood-fill/
+// Flood Fill
+// Day #25
+#include <iostream>    
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+		// We need to consider starting pixel, if it is alrealy filled with newColor then return
+		if (image[sr][sc] != newColor) {
+			int oldColor = image[sr][sc];
+			dfs(image, sr, sc, newColor, oldColor);
+		}
+		return image;
+	}
+
+	void dfs(vector<vector<int>> &image, int x, int y, int &newColor, int &oldColor) {
+		if (x >= 0 && x < image.size() && y >= 0 && y < image[0].size() && image[x][y] == oldColor) {
+			image[x][y] = newColor;
+			dfs(image, x, y + 1, newColor, oldColor);
+			dfs(image, x, y - 1, newColor, oldColor);
+			dfs(image, x - 1, y, newColor, oldColor);
+			dfs(image, x + 1, y, newColor, oldColor);
+		}
+	}
+};
+
+int main(void) {
+	vector<vector<int>> res, image = { {0, 0, 0}, {0, 1, 1} };
+	int sr = 1, sc = 1, newColor = 1;
+	//vector<vector<int>> res, image = { {1, 1, 1}, {1, 1, 0}, {1, 0, 1} };
+	//int sr = 1, sc = 1, newColor = 2;
+	Solution obj;
+	res = obj.floodFill(image, sr, sc, newColor);
+	cout << "Result: \n";
+	for (auto row : image) {
+		for (auto pixel : row) {
+			cout << pixel << " ";
+		}
+		cout << endl;
+	}
+}
