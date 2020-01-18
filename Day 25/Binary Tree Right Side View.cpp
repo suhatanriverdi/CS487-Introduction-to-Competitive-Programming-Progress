85c093ce41688febf5ed0d9e95b8e834c6f3a086
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-18 22:52:32 +0300

    Add files via upload

---
+++ Day 25/Binary Tree Right Side View.cpp
@@ -0,0 +1,30 @@
+﻿// Question Link ---> https://leetcode.com/problems/binary-tree-right-side-view/
+// Day #25
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (root == NULL) {
+            return {};
+        }
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        vector<int> rightSideNodes = { root->val };
+        while (!nodes.empty()) {
+            int levelWidth = nodes.size();
+            while (levelWidth--) {                
+                TreeNode* currentNode = nodes.front();
+                nodes.pop();
+                if (currentNode->right) {
+                    nodes.push(currentNode->right);
+                }
+                if (currentNode->left) {
+                    nodes.push(currentNode->left);
+                }
+            }
+            if (!nodes.empty()) {
+                rightSideNodes.push_back(nodes.front()->val);
+            }
+        }
+        return rightSideNodes;
+    }
+};
