105d403df4564573647873ecccd0d034660963b9
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-18 21:09:11 +0300

    Add files via upload

---
+++ Day 25/Maximum Depth of N-ary Tree - Iterative.cpp
@@ -0,0 +1,25 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+// Day #25
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int depth = 0;
+        queue<Node*> nodes;
+        nodes.push(root);
+        while (!nodes.empty()) {
+            int currentQueueSize = nodes.size();
+            while (currentQueueSize--) {      
+                Node* currentNode = nodes.front();
+                nodes.pop();
+                for (auto child : currentNode->children) {
+                    nodes.push(child);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
---
+++ Day 25/Maximum Depth of N-ary Tree - Recursive.cpp
@@ -0,0 +1,15 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+// Day #25
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int depth = 1;
+        for (auto node : root->children) {
+            depth = max(maxDepth(node) + 1, depth);
+        }
+        return depth;
+    }
+};
