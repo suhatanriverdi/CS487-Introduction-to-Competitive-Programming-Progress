a0f88e67a40117727fb45ade3cda15920ebbfe1c
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-18 22:20:29 +0300

    Add files via upload

---
+++ Day 25/Employee Importance - bfs.cpp
@@ -0,0 +1,28 @@
+﻿// Question Link ---> https://leetcode.com/problems/employee-importance
+// Day #25
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        int importanceSum = 0;
+        for (auto employee : employees) {
+            if (employee->id == id) { // Find matching employee id
+                queue<Employee*> allEmployees;
+                allEmployees.push(employee);
+                while (!allEmployees.empty()) {
+                    Employee* currentEmployee = allEmployees.front();
+                    allEmployees.pop();
+                    importanceSum += currentEmployee->importance;
+                    for (auto subordinateID : currentEmployee->subordinates) {
+                        for (auto emp : employees) {
+                            if (emp->id == subordinateID) {
+                                allEmployees.push(emp);
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        return importanceSum;
+    }
+};
---
+++ Day 25/Employee Importance - hashmap.cpp
@@ -0,0 +1,25 @@
+﻿// Question Link ---> https://leetcode.com/problems/employee-importance
+// Day #25
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> idMap;
+        for (auto employee : employees) { // Save Employee id's as a key, employee itself as a value
+            idMap.insert({employee->id, employee});
+        }
+        int importanceSum = 0;
+        queue<Employee*> allEmployees;
+        if (idMap.count(id)) {
+            allEmployees.push(idMap[id]);
+            while (!allEmployees.empty()) {
+                Employee* currentEmployee = allEmployees.front();
+                allEmployees.pop();
+                importanceSum += currentEmployee->importance;
+                for (auto subordinateID : currentEmployee->subordinates) {
+                    allEmployees.push(idMap[subordinateID]);
+                }
+            }
+        }
+        return importanceSum;
+    }
+};
