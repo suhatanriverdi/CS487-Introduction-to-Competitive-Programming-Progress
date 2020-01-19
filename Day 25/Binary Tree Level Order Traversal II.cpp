d84f259850ea05e17a2ad1b6d23b99f5c7b1d5af
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-20 02:52:14 +0300

    Add files via upload

---
+++ Day 25/Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,31 @@
+﻿// Question Link ---> https://leetcode.com/problems/rotting-oranges/
+// Day #25
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if (root == NULL) {
+            return {};
+        }
+        vector<vector<int>> levelOrderValues;
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        while (!nodes.empty()) {
+            vector<int> nodesAtSameLevel;
+            int currentQueueSize = nodes.size();
+            while (currentQueueSize--) {
+                TreeNode* currentNode = nodes.front();
+                nodesAtSameLevel.push_back(currentNode->val);
+                nodes.pop();
+                if (currentNode->left) {
+                    nodes.push(currentNode->left);
+                }
+                if (currentNode->right) {
+                    nodes.push(currentNode->right);
+                }
+            }
+            levelOrderValues.push_back(nodesAtSameLevel);
+        }
+        reverse(levelOrderValues.begin(),levelOrderValues.end());
+        return levelOrderValues;
+    }
+};
