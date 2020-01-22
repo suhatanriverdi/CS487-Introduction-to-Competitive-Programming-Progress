83f9ea44ceac765e7da7b160d2d275b611fbc3e5
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-22 21:00:53 +0300

    Add files via upload

---
+++ Day 19/Find Positive Integer Solution for a Given Equation - Bad Version.cpp
@@ -0,0 +1,16 @@
+﻿// Question Link ---> https://leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/
+// Day #19
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction &customfunction, int z) {
+        vector<vector<int>> posIntPairs;
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = 1; j <= 1000; j++) {
+                if (customfunction.f(i, j) == z) {
+                    posIntPairs.push_back({i, j});
+                }
+            }           
+        }
+        return posIntPairs;
+    }
+};
