c7a7f0b513c7234c10d6162b5a2b0b1736a9dc20
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-23 22:36:53 +0300

    Add files via upload

---
+++ Day 25/Validate Binary Search Tree.cpp
@@ -0,0 +1,28 @@
+﻿// Question Link ---> https://leetcode.com/problems/validate-binary-search-tree/
+// Day #25
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+        vector<int> values;
+        traveseInOrder(root, values);
+        for (int i = 0; i + 1 < values.size(); i++) {
+            if (values[i] >= values[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    void traveseInOrder(TreeNode *cur, vector<int> &values) {
+        if (cur == NULL) {
+            return;
+        }
+        traveseInOrder(cur->left, values);
+        values.push_back(cur->val);
+        traveseInOrder(cur->right, values);
+    }
+
+};
