00e7a4944e5ff02422cae1bcb1734b59e899cee0
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-25 19:51:03 +0300

    Add files via upload

---
+++ Day 24/Design HashMap.cpp
@@ -0,0 +1,25 @@
+// Question Link ---> https://leetcode.com/problems/design-hashmap/
+// Day #24
+class MyHashMap {
+public:
+    int map[1000001]; // all elements -1
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        fill(map.begin(), map.end(), -1);
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return map[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        map[key] = -1;
+    }
+};
