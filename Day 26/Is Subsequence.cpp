77e2ff4bbe1b24c7a97be6506326e7c885050c21
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-27 12:41:36 +0300

    Add files via upload

---
+++ Day 26/Is Subsequence.cpp
@@ -0,0 +1,14 @@
+﻿// Question Link ---> https://leetcode.com/problems/is-subsequence/
+// Day #26
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        while (t.size() > 0 && s.size() > 0) {
+            if (s.back() == t.back()) {
+                s.pop_back();
+            }
+            t.pop_back();
+        }
+        return s.size() == 0;
+    }
+};
