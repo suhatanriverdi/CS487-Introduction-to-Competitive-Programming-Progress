ece78c651bd818810e15ba436ad75fdb527f2163
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-30 14:06:01 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 112/Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,20 @@
+﻿// Question Link ---> https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& A) {
+        int res = 0, recent;
+        sort(A.begin(), A.end());
+        unordered_set<int> uniqs;
+        for (int i = 0; i < A.size(); i++) {
+            if (!uniqs.count(A[i])) {
+                recent = A[i];
+                uniqs.insert(A[i]);
+            } else {
+                recent++;
+                uniqs.insert(recent);
+                res += recent - A[i];
+            }
+        }
+        return res;
+    }
+};
