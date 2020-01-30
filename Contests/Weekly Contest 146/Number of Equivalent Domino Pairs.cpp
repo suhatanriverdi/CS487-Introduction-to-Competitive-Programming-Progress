e295861c660be3c2716635d3e647ff747a1a3e60
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-30 16:31:04 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 146/Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,34 @@
+﻿// Question Link ---> https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int cnt = 0;
+        map<pair<int, int>, int> m, equ;
+        unordered_set<int> s;
+        for (int i = 0; i < dominoes.size(); i++) {
+            if (dominoes[i][0] != dominoes[i][1]) {
+                m[{dominoes[i][0], dominoes[i][1]}]++;
+                m[{dominoes[i][1], dominoes[i][0]}]++;
+            } else {
+                equ[{dominoes[i][1], dominoes[i][0]}]++;
+            }
+        }
+        for (auto i : m) {
+            if (i.second > 1) {
+                if (!s.count(i.second)) {
+                    s.insert(i.second);
+                    cnt += ((i.second * (i.second - 1)) / 2);
+                }
+                else {
+                    s.erase(i.second); 
+                }
+            }
+        }
+        for (auto i : equ) {
+            if (i.second > 1) {
+                cnt += ((i.second * (i.second - 1)) / 2);
+            }
+        }
+        return cnt;
+    }
+};
