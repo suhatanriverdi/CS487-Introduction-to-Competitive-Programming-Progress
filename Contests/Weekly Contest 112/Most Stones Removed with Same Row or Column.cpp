cc8aede0a5be260e99d4595fa49df1a37332df25
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-01-31 18:56:44 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 112/Most Stones Removed with Same Row or Column.cpp
@@ -0,0 +1,28 @@
+﻿// Question Link ---> https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+class Solution {
+public:
+	int removeStones(vector<vector<int>>& stones) {
+		queue<pair<int, int>> nodes;
+		int largestPossibleNumOfMoves = 0, curMaxMoves;
+		for (int i = 0; i < stones.size(); i++) {
+			if (stones[i][0] != -1) {
+				nodes.push({ stones[i][0], stones[i][1] });
+				stones[i][0] = -1; // Mark As Visited
+			}
+			curMaxMoves = 0;
+			while (!nodes.empty()) {
+				auto cur = nodes.front(); nodes.pop();
+				for (int i = 0; i < stones.size(); i++) {
+					int nx = stones[i][0], ny = stones[i][1];
+					if (nx != -1 && (nx == cur.first || ny == cur.second)) {
+						nodes.push({ stones[i][0], stones[i][1] });
+						stones[i][0] = -1; // Mark as visited
+						curMaxMoves += 1;
+					}
+				}
+			}
+			largestPossibleNumOfMoves += curMaxMoves;
+		}
+		return largestPossibleNumOfMoves;
+	}
+};
