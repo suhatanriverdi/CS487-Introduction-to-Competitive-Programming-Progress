321b36b8ae0094b69dbec0a336261e17f5e20d65
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-01 12:54:38 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 112/Bag of Tokens.cpp
@@ -0,0 +1,23 @@
+﻿// Question Link ---> https://leetcode.com/problems/bag-of-tokens/
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int P) {
+        sort(tokens.begin(), tokens.end()); // 1 3 5
+        bool playedFaceUp = true, playedFaceDown = true;
+        int points = 0, maxIndex = tokens.size() - 1, minIndex = 0;
+        while (minIndex <= maxIndex && P > 0 && (playedFaceUp || playedFaceDown)) {
+            playedFaceUp = playedFaceDown = false;
+            while (minIndex < tokens.size() && P >= tokens[minIndex]) {
+                P -= tokens[minIndex++];
+                points++;
+                playedFaceUp = true;
+            }
+            if (maxIndex >= 0 && minIndex < maxIndex && points > 0) {
+                P += tokens[maxIndex--];
+                points--;
+                playedFaceDown = true;
+            }
+        }
+        return points;
+    }
+};
