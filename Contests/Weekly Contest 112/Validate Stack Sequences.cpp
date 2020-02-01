40231707f3a543eb056889d73ffd5c4908aa632c
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-01 11:53:54 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 112/Validate Stack Sequences.cpp
@@ -0,0 +1,16 @@
+﻿// Question Link ---> https://leetcode.com/problems/validate-stack-sequences/
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int j = 0;
+        stack<int> validateStack;
+        for (auto valPush : pushed) {
+            validateStack.push(valPush);
+            while (!validateStack.empty() && j < popped.size() && validateStack.top() == popped[j]) {
+                j++;
+                validateStack.pop();
+            }
+        }
+        return validateStack.empty();
+    }
+};
