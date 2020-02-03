caee33779841452533395714708a480547e66705
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-03 17:57:50 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 100/Increasing Order Search Tree.cpp
@@ -0,0 +1,34 @@
+﻿// Question Link ---> https://leetcode.com/problems/increasing-order-search-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        if (root == NULL) {
+            return root;
+        }
+        vector<int> nums;
+        inOrder(root, nums);
+        TreeNode *newRoot = new TreeNode(nums[0]);
+        TreeNode *walk = newRoot;
+        for (int i = 1; i < nums.size(); i++) {
+            walk->right = new TreeNode(nums[i]);
+            walk = walk->right;
+        }
+        return newRoot;
+    }
+    
+    void inOrder(TreeNode* cur, vector<int> &nums) {
+        if (cur == NULL) return;
+        inOrder(cur->left, nums);
+        if (cur != NULL) nums.push_back(cur->val);
+        inOrder(cur->right, nums);
+    }
+};
---
+++ Contests/Weekly Contest 100/Monotonic Array.cpp
@@ -0,0 +1,34 @@
+﻿// Question Link ---> https://leetcode.com/problems/monotonic-array/
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        if (A.size() == 1) {
+            return true;
+        }
+        if (isIncreasing(A)) {
+            return true;
+        }
+        if (isDecreasing(A)) {
+            return true;
+        }
+        return false;
+    }
+    
+    bool isIncreasing(vector<int> &A) {
+        for (int i = 0; i + 1 < A.size(); i++) {
+            if (A[i] > A[i + 1]) {
+                return false;  
+            }
+        }
+        return true;
+    }
+    
+    bool isDecreasing(vector<int> &A) {
+        for (int i = 0; i + 1 < A.size(); i++) {
+            if (A[i] < A[i + 1]) {
+                return false;  
+            }
+        }
+        return true;
+    }
+};
