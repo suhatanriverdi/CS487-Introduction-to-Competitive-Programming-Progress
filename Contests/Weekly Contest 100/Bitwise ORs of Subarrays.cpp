1305fa277700f328f4b02d9b26208449c31b242b
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-05 21:48:00 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 100/Bitwise ORs of Subarrays.cpp
@@ -0,0 +1,18 @@
+﻿// Question Link ---> https://leetcode.com/problems/bitwise-ors-of-subarrays/
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& A) {
+        unordered_set<int> res, prev;
+        for (auto cur : A) {
+            unordered_set<int> temp = {cur};
+            for (auto i : prev) {
+                temp.insert(i | cur);
+            }
+            for (auto i : temp) {
+                res.insert(i | cur);
+            }
+            prev = temp;
+        }
+        return res.size();
+    }
+};
