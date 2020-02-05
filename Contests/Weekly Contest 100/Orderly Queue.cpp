fa90efb937a4b5fc00b285c3131de7191ff25ee2
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-05 21:08:45 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 100/Orderly Queue.cpp
@@ -0,0 +1,26 @@
+﻿// Question Link ---> https://leetcode.com/problems/orderly-queue/
+class Solution {
+public:
+    string orderlyQueue(string S, int K) {
+        if (K == 1) {
+            string curNewStr = getMovedStr(S);
+            string minStr = min(curNewStr, S);
+            while (curNewStr != S) {
+                curNewStr = getMovedStr(curNewStr);
+                minStr = min(curNewStr, minStr);
+            }
+            return minStr;
+        }
+        sort(S.begin(), S.end());
+        return S;
+    }
+    
+    string getMovedStr(string &oldStr) {
+        string newStr = "";
+        for (int i = 1; i < oldStr.size(); i++) {
+            newStr += oldStr[i];
+        }
+        newStr += oldStr[0];
+        return newStr;
+    }
+};
