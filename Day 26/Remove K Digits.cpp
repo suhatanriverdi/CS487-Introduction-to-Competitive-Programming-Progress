15699d833f9351024db56e54f8e28ad6e794d09e
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-05 11:24:38 +0300

    Add files via upload

---
+++ Day 26/Remove K Digits.cpp
@@ -0,0 +1,31 @@
+﻿// Question Link ---> https://leetcode.com/problems/remove-k-digits/
+// Day #26
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (k == num.size()) {
+            return "0";
+        }
+        string mayHaveLeadingZeros = "", result = "";
+        for (int i = k; i < num.size(); i++) {
+            mayHaveLeadingZeros += findSmallestInLeft(num, i);
+        }
+        int i = 0;
+        while (i < mayHaveLeadingZeros.size() && mayHaveLeadingZeros[i] == '0') i++;
+        for (int j = i; j < mayHaveLeadingZeros.size(); j++) result += mayHaveLeadingZeros[j];
+        return (result.size() == 0 ? "0" : result);
+    }
+    
+    char findSmallestInLeft(string &str, int index) {
+        char localMinChar = str[index];
+        int localMinIndex = index;
+        for (int i = index; i>= 0 && str[i] != '.'; i--) {
+            if (str[i] <= localMinChar) {
+                localMinIndex = i;
+                localMinChar = str[i];
+            }
+        }
+        str[localMinIndex] = '.';
+        return localMinChar;
+    }
+};
