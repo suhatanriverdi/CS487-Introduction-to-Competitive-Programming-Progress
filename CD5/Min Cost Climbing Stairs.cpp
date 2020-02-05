c5d350c9a4c61de4cfadae5f9720cdc598dade74
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-05 14:29:37 +0300

    Add files via upload

---
+++ CD5/Min Cost Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+﻿// Question Link ---> https://leetcode.com/problems/min-cost-climbing-stairs/
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        if (cost.size() == 2) {
+            return min(cost[0], cost[1]);
+        }
+        for (int i = 2; i < cost.size(); i++) {
+            cost[i] += min(cost[i - 1], cost[i - 2]);
+        }
+        int size = cost.size() - 1;
+        return min(cost[size], cost[size - 1]);
+    }
+};
