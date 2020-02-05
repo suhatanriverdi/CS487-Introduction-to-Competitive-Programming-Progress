853094af2fe53ba435eac8a699cb9e48643db29b
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-06 00:43:09 +0300

    Add files via upload

---
+++ CD5/Range Sum Query - Immutable.cpp
@@ -0,0 +1,20 @@
+﻿// Question Link ---> https://leetcode.com/problems/range-sum-query-immutable/
+class NumArray {
+public:
+    vector<int> accu = {0};
+    NumArray(vector<int> &nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            accu.push_back(nums[i] + accu.back());
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return accu[j + 1] - accu[i];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
