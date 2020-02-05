7177d753bc5ac245ff90c9755d61e56c52c11f98
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-05 22:02:24 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 100/Maximum Subarray.cpp
@@ -0,0 +1,17 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximum-subarray
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curMax = INT_MIN, sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum > curMax) {
+                curMax = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return curMax;
+    }
+};
