c8fe9a54b8bc1e97db60c97b80be12fbda02a781
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-05 23:55:29 +0300

    Add files via upload

---
+++ CD5/House Robber.cpp
@@ -0,0 +1,15 @@
+﻿// Question Link ---> https://leetcode.com/problems/house-robber/
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        if (size <= 2) {
+            return (size == 1 ? nums[0] : (size == 0 ? 0 : max(nums[0], nums[1])));
+        }
+        for (int i = 2; i < nums.size(); i++) {
+            nums[i] = max(nums[i - 1], nums[i] + nums[i - 2]);
+            nums[i - 1] = max(nums[i - 2], nums[i - 1]);
+        }
+        return nums[size - 1];
+    }
+};
