f679f00126d7f1b67e24e9c0d051044b72ac07a2
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-06 18:39:25 +0300

    Add files via upload

---
+++ CD6/Reduce Array Size to The Half.cpp
@@ -0,0 +1,21 @@
+﻿// Question Link ---> https://leetcode.com/problems/reduce-array-size-to-the-half/
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        vector<int> orderedFreqs;
+        unordered_map<int, int> freqs; // {num, freq}
+        for (auto i : arr) freqs[i]++;
+        if (freqs.size() == 1) return 1;
+        for (auto i : freqs) orderedFreqs.push_back(i.second);
+        int half = arr.size() / 2, sum = 0, setCnt = 0; 
+        sort(orderedFreqs.begin(), orderedFreqs.end(), greater<int>());
+        for (auto curFreq : orderedFreqs) {
+            sum += curFreq;
+            setCnt++;
+            if (sum >= half) {
+                return setCnt;
+            }
+        }
+        return setCnt;
+    }
+};
---
+++ CD6/The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,24 @@
+﻿// Question Link ---> https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<int> res;
+        multimap<int, int> soldierRow; // {soldier, row}
+        int M = mat.size();
+        int N = mat[0].size();
+        for (int i = 0; i < M; i++) {
+            int j = 1;
+            for (; j < N; j++) {
+                if (mat[i][j] != 0) {
+                    mat[i][j] += mat[i][j - 1];
+                } else break;
+            }
+            soldierRow.insert({mat[i][j - 1], i});
+        }
+        auto it = soldierRow.begin();
+        for (int i = 0; i < k && it != soldierRow.end(); i++, it++) {
+            res.push_back(it->second);
+        }
+        return res;
+    }
+};
