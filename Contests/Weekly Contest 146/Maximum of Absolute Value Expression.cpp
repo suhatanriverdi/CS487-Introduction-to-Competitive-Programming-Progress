8b3f1af0af21ce18915fc2e476a49fff5775097d
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-09 21:01:32 +0300

    Add files via upload

---
+++ Contests/Weekly Contest 146/Maximum of Absolute Value Expression.cpp
@@ -0,0 +1,21 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximum-of-absolute-value-expression/
+class Solution {
+public:
+    int maxAbsValExpr(vector<int>& arr1, vector<int>& arr2) {
+        int res = 0;
+        res = max(loopOver(1, 1, arr1, arr2), res);
+        res = max(loopOver(-1, -1, arr1, arr2), res);
+        res = max(loopOver(1, -1, arr1, arr2), res);
+        res = max(loopOver(-1, 1, arr1, arr2), res);
+        return res;
+    }
+    
+    int loopOver(int s1, int s2, vector<int>& arr1, vector<int>& arr2) {
+        int maX = INT_MIN, miN = INT_MAX;
+        for (int i = 0; i < arr1.size(); i++) {
+            maX = max(maX, s1 * arr1[i] + s2 * arr2[i] + i);
+            miN = min(miN, s1 * arr1[i] + s2 * arr2[i] + i);
+        }
+        return maX - miN;
+    }
+};
