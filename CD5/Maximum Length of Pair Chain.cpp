1f336a087492a2fa8da85ea95ed9748eeb48d17a
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-10 10:15:06 +0300

    Add files via upload

---
+++ CD5/Maximum Length of Pair Chain.cpp
@@ -0,0 +1,19 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximum-length-of-pair-chain/
+class Solution {
+private:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+        return a[1] < b[1];
+    }
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int cnt = 0, prev = INT_MIN;
+        sort(pairs.begin(), pairs.end(), cmp);
+        for (int i = 0; i < pairs.size(); i++) {
+            if (prev < pairs[i][0]) {
+                prev = pairs[i][1];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
