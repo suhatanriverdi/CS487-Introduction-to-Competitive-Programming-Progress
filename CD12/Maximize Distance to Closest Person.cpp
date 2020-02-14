779a75ca20dc39347d4fa684e5f1ef77297a9cbe
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-14 14:32:36 +0300

    Add files via upload

---
+++ CD12/Maximize Distance to Closest Person.cpp
@@ -0,0 +1,28 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximize-distance-to-closest-person/
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        queue<int> onesQueue;
+        int size = seats.size(), maxDist = 1;
+        for (int i = 0; i < size; i++) {
+            if (seats[i] == 1) {
+                onesQueue.push(i);
+            }
+        }
+        while(!onesQueue.empty()) {
+            int cur = onesQueue.front();
+            onesQueue.pop();
+            if (cur + 1 < size && seats[cur + 1] == 0) {
+                onesQueue.push(cur + 1);
+                seats[cur + 1] = seats[cur] + 1;
+                maxDist = max(maxDist, seats[cur + 1]);
+            }
+            if (cur - 1 >= 0 && seats[cur - 1] == 0) {
+                onesQueue.push(cur - 1);
+                seats[cur - 1] = seats[cur] + 1;
+                maxDist = max(maxDist, seats[cur - 1]);
+            }
+        }
+        return maxDist - 1;
+    }
+};
---
+++ CD12/Shifting Letters.cpp
@@ -0,0 +1,25 @@
+﻿// Question Link ---> https://leetcode.com/problems/shifting-letters/
+class Solution {
+public:
+    string shiftingLetters(string S, vector<int>& shifts) {
+        int size = S.size();
+        vector<long long int> dp(size, 0);
+        for (int i = 0; i < size; i++) {
+            dp[i] = shifts[i];
+        }
+        for (int i = size - 2; i >= 0; i--) {
+            dp[i] += dp[i + 1];
+        }
+        for (int i = 0; i < size; i++) {
+            int shiftAmount = dp[i] % 26;
+            while (shiftAmount-- > 0) {
+                if (S[i] == 122) {
+                    S[i] = 97;
+                } else {
+                    S[i]++;
+                }
+            }
+        }
+        return S;
+    }
+};
