4eaeb8aa112dd6e149d8644e761115eeb51e0999
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-17 14:41:36 +0300

    Add files via upload

---
+++ CD12/Count Negative Numbers in a Sorted Matrix.cpp
@@ -0,0 +1,13 @@
+﻿// Question Link ---> https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+    	int negativeCount = 0;
+        for (auto row : grid) {
+        	for (int i = row.size() - 1; i >= 0 && row[i] < 0; i--) {
+        		negativeCount++;
+        	}
+        }
+        return negativeCount;
+    }
+};
---
+++ CD12/Product of the Last K Numbers.cpp
@@ -0,0 +1,24 @@
+﻿// Question Link ---> https://leetcode.com/problems/product-of-the-last-k-numbers/
+class ProductOfNumbers {
+public:
+    vector<int> prods = {1};
+    ProductOfNumbers() {
+        
+    }
+
+    void add(int num) {
+   		if (num == 0) {
+   			prods = {1};
+   		}
+   		else {
+   			prods.push_back(prods.back() * num);
+   		}
+    }
+    
+    int getProduct(int k) {
+    	if (k > prods.size() - 1) {
+    		return 0;
+    	}
+		return prods.back() / prods[prods.size() - 1 - k];
+    }
+};
