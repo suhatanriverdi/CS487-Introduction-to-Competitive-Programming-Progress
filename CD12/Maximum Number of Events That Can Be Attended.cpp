73de26957fe0b68cd80742772491d180923010b4
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-17 15:50:37 +0300

    Add files via upload

---
+++ CD12/Maximum Number of Events That Can Be Attended.cpp
@@ -0,0 +1,24 @@
+﻿// Question Link ---> https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
+class Solution {
+public:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+        if (a[1] == b[1]) {
+            return a[0] < b[0];
+        }
+        return a[1] < b[1];
+    }
+
+    int maxEvents(vector<vector<int>>& events) {
+        unordered_set<int> attended;
+        sort(events.begin(), events.end(), cmp);
+        for (auto event : events) {
+            for (int i = event[0]; i <= event[1]; i++) {
+                if (!attended.count(i)) {
+                    attended.insert(i);
+                    break;
+                }
+            }
+        }
+        return attended.size();
+    }
+};
