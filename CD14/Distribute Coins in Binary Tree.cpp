016a0f18881aae57f1f1c7244b3bee1f1dd37710
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-21 15:47:53 +0300

    Add files via upload

---
+++ CD14/Distribute Coins in Binary Tree.cpp
@@ -0,0 +1,57 @@
+﻿// Question Link ---> https://leetcode.com/problems/distribute-coins-in-binary-tree/
+// C++ Version 1 No Global Variable
+class Solution {
+public:
+    int distributeCoins(TreeNode* root) {
+        int diff = 0;
+        postOrderDistribute(root, diff);
+        return diff;
+    }
+    
+    int postOrderDistribute(TreeNode *cur, int &diff) {
+        if (cur == NULL) {
+            return 0;
+        }
+        cur->val += postOrderDistribute(cur->left, diff) +
+        postOrderDistribute(cur->right, diff);
+        diff += abs(cur->val - 1);
+        return (cur->val - 1);
+    }
+};
+
+// Java Version 1 No Global Variable
+class Solution {
+    public int distributeCoins(TreeNode root) {
+        int[] diff = {0};
+        postOrderDistribute(root, diff);
+        return diff[0];
+    }
+    
+    public int postOrderDistribute(TreeNode cur, int[] diff) {
+        if (cur == null) {
+            return 0;
+        }
+        cur.val += postOrderDistribute(cur.left, diff) + postOrderDistribute(cur.right, diff);
+        diff[0] += Math.abs(cur.val - 1);
+        return (cur.val - 1);
+    }
+}
+
+// Java Version 2 by Using Global Variable
+class Solution {
+    int diff;
+    public int distributeCoins(TreeNode root) {
+        diff = 0;
+        postOrderDistribute(root);
+        return diff;
+    }
+    
+    public int postOrderDistribute(TreeNode cur) {
+        if (cur == null) {
+            return 0;
+        }
+        cur.val += postOrderDistribute(cur.left) + postOrderDistribute(cur.right);
+        diff += Math.abs(cur.val - 1);
+        return (cur.val - 1);
+    }
+}
---
+++ CD14/Squares of a Sorted Array.cpp
@@ -0,0 +1,11 @@
+﻿// Question Link ---> https://leetcode.com/problems/squares-of-a-sorted-array/
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        for (auto &val : A) {
+            val = pow(val, 2);
+        }
+        sort(A.begin(), A.end());
+        return A;
+    }
+};
