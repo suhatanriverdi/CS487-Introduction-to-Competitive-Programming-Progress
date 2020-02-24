d1dd87397da81221b36b96b990b3a5aed674d6a0
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-02-24 13:34:17 +0300

    Add files via upload

---
+++ CD13/Linked List Components.cpp
@@ -0,0 +1,33 @@
+﻿// Question Link ---> https://leetcode.com/problems/linked-list-components/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& G) {
+        unordered_set<int> possibleConnecteds;
+        for (auto val : G) {
+            possibleConnecteds.insert(val);
+        }
+        if (head == NULL || head->next == NULL) {
+            return (head == NULL ? 0 : (possibleConnecteds.count(head->val) > 0 ? 1 : 0));
+        }
+        int cnt = 0;
+        ListNode *walk = head;
+        while (walk != NULL) {
+            if (possibleConnecteds.count(walk->val)) {
+                cnt++;
+            }
+            while (walk->next && possibleConnecteds.count(walk->val) && possibleConnecteds.count(walk->next->val)) {
+                walk = walk->next; 
+            }
+            walk = walk->next;
+        }
+        return cnt;
+    }
+};
---
+++ CD13/Most Common Word.cpp
@@ -0,0 +1,36 @@
+﻿// Question Link ---> https://leetcode.com/problems/most-common-word/
+class Solution {
+public:
+    string findMaxFreq(unordered_map<string, int> &wordFreqs) {
+        int maX = 0;
+        string ans = "";
+        for (auto pair : wordFreqs) {
+            if (pair.second > maX) {
+                maX = pair.second;
+                ans = pair.first;
+            }
+        }
+        return ans;
+    }
+    
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_map<string, int> wordFreqs;
+        unordered_set<char> puncs = {'!', '?', '\'', ',', ';', '.', ' '};
+        unordered_set<string> bannedWords;
+        for (auto banWord : banned) {
+            bannedWords.insert(banWord);
+        }
+        for (int i = 0; i < paragraph.size(); i++) {
+            string tmp = "";
+            while (i < paragraph.size() && !puncs.count(paragraph[i])) {
+                tmp += paragraph[i];
+                i++;
+            }
+            transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
+            if (!bannedWords.count(tmp) && tmp != "") {
+                wordFreqs[tmp]++;
+            }
+        }
+        return findMaxFreq(wordFreqs);
+    }
+};
