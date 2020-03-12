adddd5eb3203325ad519838d2852d65338611e4a
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-03-12 23:10:54 +0300

    Add files via upload

---
+++ SH1/Generate a String With Characters That Have Odd Counts.cpp
@@ -0,0 +1,13 @@
+﻿// Question Link ---> https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+class Solution {
+public:
+    string generateTheString(int n) {
+        string res(n,'a');
+        if (n & 1) { // If the number is odd
+            return res;
+        }
+        // If the number is even
+        res[n - 1] = 'b';
+        return res;
+    }
+};
