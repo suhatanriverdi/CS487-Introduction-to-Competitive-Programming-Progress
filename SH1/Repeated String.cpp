c2229211c307836d95b8dab807fd2167555912a4
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-03-16 11:54:02 +0300

    Add files via upload

---
+++ SH1/Repeated String.cpp
@@ -0,0 +1,17 @@
+﻿// Question Link ---> https://www.hackerrank.com/challenges/repeated-string/problem
+long repeatedString(string s, long n) {
+    long long int strSize = s.size();
+    long long int cntA = 0, remain = n % strSize, repeat = n / strSize;
+    for (char c : s) {
+        if (c == 'a') {
+            cntA++;
+        }
+    }
+    cntA = repeat * cntA;
+    for (long int i = 0; remain > 0; remain--, i++) {
+        if (s[i] == 'a') {
+            cntA++;
+        }
+    }
+    return cntA;
+}
