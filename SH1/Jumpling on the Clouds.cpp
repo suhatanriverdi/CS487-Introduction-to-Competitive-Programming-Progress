141f412bbbb25c91261fcb8b61ec09f5738774a8
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-03-16 11:10:36 +0300

    Add files via upload

---
+++ SH1/Jumpling on the Clouds.cpp
@@ -0,0 +1,15 @@
+﻿// Question Link ---> https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+int jumpingOnClouds(vector<int> c) {
+    int cnt = 0, size = c.size(), i = 0;
+    while (i < size - 1) {
+        if (i + 2 < size && c[i + 2] == 0) {
+            i += 2;
+            cnt++;
+        }
+        else if (i + 1 < size && c[i + 1] == 0) {
+            i += 1;
+            cnt++;
+        }
+    }
+    return cnt;
+}
