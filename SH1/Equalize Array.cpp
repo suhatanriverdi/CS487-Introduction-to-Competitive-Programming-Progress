84684ac3bd4f7278a0c93e48429e272e645220e6
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-03-18 15:09:15 +0300

    Add files via upload

---
+++ SH1/Equalize Array.cpp
@@ -0,0 +1,12 @@
+﻿// Question Link ---> https://www.hackerrank.com/challenges/equality-in-a-array/problem
+int equalizeArray(vector<int> arr) {
+    int numOfVals = arr.size(), maxFreq = INT_MIN;
+    unordered_map<int, int> freqs;
+    for (int num : arr) {
+        freqs[num]++;
+    }
+    for (auto freq : freqs) {
+        maxFreq = max(maxFreq, freq.second);
+    }
+    return numOfVals - maxFreq;
+}
