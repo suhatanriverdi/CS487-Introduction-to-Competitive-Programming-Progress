58dac3074c84d8ff1434da128a07b9a8c696ee80
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-03-19 12:58:50 +0300

    Add files via upload

---
+++ SH1/Bulb Switcher III.cpp
@@ -0,0 +1,15 @@
+﻿// Question Link ---> https://leetcode.com/problems/bulb-switcher-iii/
+class Solution {
+public:
+    int numTimesAllBlue(vector<int> &light) {
+        int rIdx = 0, cnt = 0, openCnt = 0;
+        for (int i = 0; i < light.size(); i++) {
+            openCnt++;
+            rIdx = max(rIdx, light[i]);
+            if (openCnt == rIdx) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
