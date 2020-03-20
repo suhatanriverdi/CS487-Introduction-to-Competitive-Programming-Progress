0d7a4fa4346ee08d9b2b2f628c3ffab7f3f81166
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2020-03-20 14:01:36 +0300

    Add files via upload

---
+++ SH1/Find Median from Data Stream.cpp
@@ -0,0 +1,23 @@
+﻿// Question Link ---> https://leetcode.com/problems/find-median-from-data-stream/submissions/
+class MedianFinder {
+	multiset<int> numSet;
+	multiset<int>::iterator it;
+	int size;
+	
+public:
+    void addNum(int num) {
+         numSet.insert(num);
+    }
+    
+    double findMedian() {
+        it = numSet.begin();
+        size = numSet.size();
+        advance(it, size/2);
+        if (size & 1) { // Odd
+            return *it;
+        } 
+        else { // Even
+            return (double)(*--it + *++it) / 2;
+        }
+    }
+};
